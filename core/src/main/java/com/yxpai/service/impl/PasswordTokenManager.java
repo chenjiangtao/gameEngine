85f443ba2b042dbb8eef8dae6e3bca88e7a04c98
jiangtao <jiangtao.chen@yxpai.com>
2014-09-23 16:16:27 +0800

    init

---
+++ core/src/main/java/com/yxpai/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.yxpai.service.impl;
+
+import com.yxpai.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
